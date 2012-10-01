ef8d4393f015484fc272399a8d8a90097d31f3d7
BMRagot <bastien.marichal@gmail.com>
2012-10-01 16:23:55 +0200

    Add last classes

---
+++ TPObjetLofter/src/Cannibale.java
@@ -0,0 +1,12 @@
+/**
+ * 1 oct. 2012
+ * Cannibale.java
+ */
+
+/**
+ * @author bastienmarichalragot
+ *
+ */
+public class Cannibale extends Vorace{
+
+}
---
+++ TPObjetLofter/src/Vorace.java
@@ -0,0 +1,12 @@
+/**
+ * 1 oct. 2012
+ * Vorace.java
+ */
+
+/**
+ * @author bastienmarichalragot
+ *
+ */
+public class Vorace extends Erratique{
+
+}
